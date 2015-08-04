fcd0b071624274057da10f46cffe93ab05a7dd46
Denis Toledo <contato@denistoledo.com.br>
2015-08-03 21:16:41 -0300

    gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp"),
-	less = require("gulp-less");
+	less = require("gulp-less"),
+	minify = require("gulp-minify-css");
 
 gulp.task("compile-less", function() {
 	gulp.src("dev/less/main.less")
 		.pipe(less())
+		.pipe(minify())
 		.pipe(gulp.dest("dist/css"))
 });
 
