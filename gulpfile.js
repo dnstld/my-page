5cef83996406a1c9d48b251d1302da59f953caef
Denis Toledo <contato@denistoledo.com.br>
2015-08-03 22:16:39 -0300

    gulp-scripts-taks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require("gulp"),
 	less = require("gulp-less"),
 	rename = require("gulp-rename"),
 	minify = require("gulp-minify-css"),
-	plumber = require("gulp-plumber");
+	plumber = require("gulp-plumber"),
+	uglify = require("gulp-uglify"),
+	concat = require("gulp-concat");
 
 gulp.task("compile-less", function() {
 	gulp.src("dev/less/main.less")
@@ -15,4 +17,17 @@ gulp.task("compile-less", function() {
 		.pipe(gulp.dest("dist/css"))
 });
 
-gulp.task("default", ["compile-less"]);
+gulp.task("scripts", function() {
+	gulp.src("dev/js/main.js")
+		.pipe(plumber())
+		.pipe(concat("main.js"))
+		.pipe(uglify().on("error", function() {
+			console.log(err);
+		}))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(gulp.dest("dist/js/"))
+});
+
+gulp.task("default", ["compile-less", "scripts"]);
