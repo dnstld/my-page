247b5156529028df14d7daf1a366acb274d4d5c7
Denis Toledo <contato@denistoledo.com.br>
2015-08-03 21:22:30 -0300

    gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp"),
 	less = require("gulp-less"),
+	rename = require("gulp-rename"),
 	minify = require("gulp-minify-css");
 
 gulp.task("compile-less", function() {
 	gulp.src("dev/less/main.less")
 		.pipe(less())
 		.pipe(minify())
+		.pipe(rename({
+			suffix: ".min"
+		}))
 		.pipe(gulp.dest("dist/css"))
 });
 
