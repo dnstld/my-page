928262eb4b9fc1f3763e00a66bfee40ec845bd96
Denis Toledo <contato@denistoledo.com.br>
2015-08-03 21:58:45 -0300

    gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp"),
 	less = require("gulp-less"),
 	rename = require("gulp-rename"),
-	minify = require("gulp-minify-css");
+	minify = require("gulp-minify-css"),
+	plumber = require("gulp-plumber");
 
 gulp.task("compile-less", function() {
 	gulp.src("dev/less/main.less")
+		.pipe(plumber())
 		.pipe(less())
 		.pipe(minify())
 		.pipe(rename({
